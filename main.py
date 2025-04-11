eaa5400fd34056407c097c0540d30a9483c84172
A <krusts31@gmail.com>
2025-04-11 10:34:20 +0300

    added python to gitingore added user proto files and also python functions that create users from proto files

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys 
 import account_pb2 as account_pb
+import user_pb2 as user_pb
 
 def account():
 	return account_pb.Account(
@@ -8,11 +9,31 @@ def account():
 		is_verified=True,
 		follow_ids=[0,1]
 	)
+
+def user():
+	return user_pb.User(
+		id=42,
+		name='Linus_Torvals',
+		follows=[
+			user_pb.User(id=0, name="Linux Foundation"),
+			user_pb.User(id=1, name="Dude Nukem"),
+		]
+	)
+
+def user2():
+	u = user_pb.User()
+	u.id = 42
+	u.name = 'Linux Tox'
+	u.follows.add(id=0, name="Linux Foundation")
+	u.follows.add(id=1, name="Dan Blizzard")
+	return u
 	
 
 if __name__ == '__main__':
 	fns = {
-		'account': account	
+		'account': account,
+		'user': user,
+		'user2': user2,
 	}
 	if len(sys.argv) != 2:
 		print(f'Usage: main.py [{"|".join(fns)}]')
